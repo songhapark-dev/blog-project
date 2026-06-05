c7fe43f3ec4303d22f5234da9fce75ec79cb8522
Songha Park <songhapark.pharm@gmail.com>
2026-06-05 15:38:36 +0200

    Fix: App.css removed

--- blog-frontend/src/App.jsx
+++ blog-frontend/src/App.jsx
@@ -1,5 +1,4 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import './App.css';
 import Header from './components/Header';
 import MainPage from './pages/MainPage';
 
