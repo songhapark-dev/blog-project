490cae32667b297264598247ac58bdcf89a9b2bf
Songha Park <songhapark.pharm@gmail.com>
2026-06-14 11:03:21 +0200

    Refactor: 기존 useStore 통합 스토어 파일에 JWT 인증 전역 상태 결합-오타

--- blog-frontend/src/pages/LoginPage.jsx
+++ blog-frontend/src/pages/LoginPage.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
-import { useAuthStore } from '../store/store';
+import { useStore } from '../store/store';
 
 function LoginPage() {
   const [username, setUsername] = useState('');
